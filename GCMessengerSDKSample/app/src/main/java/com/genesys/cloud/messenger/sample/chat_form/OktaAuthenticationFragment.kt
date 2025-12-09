76635e009dcc58792d1171f3d0ab773e43caab7d
Janos Kernacs <Janos.Kernacs@genesys.com>
2025-12-09 19:50:38 +0100

    GMMS-10530 Login with Implicit Flow

--- GCMessengerSDKSample/app/src/main/java/com/genesys/cloud/messenger/sample/chat_form/OktaAuthenticationFragment.kt
+++ GCMessengerSDKSample/app/src/main/java/com/genesys/cloud/messenger/sample/chat_form/OktaAuthenticationFragment.kt
@@ -1,12 +1,15 @@
 package com.genesys.cloud.messenger.sample.chat_form
 
+import android.net.Uri
 import android.view.View
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.activityViewModels
 import com.genesys.cloud.messenger.sample.BuildConfig
+import com.genesys.cloud.messenger.sample.chat_form.OktaAuthenticationFragment.Companion.TAG
 import com.genesys.cloud.messenger.sample.data.repositories.JsonSampleRepository
 
 class OktaAuthenticationFragment : WebFragment() {
@@ -35,6 +38,13 @@ class OktaAuthenticationFragment : WebFragment() {
                 request?.url?.getQueryParameter("code")?.let {
                     authCodeReceived(it)
                     return true
+                }  ?: run {
+                    request?.url?.getParam("id_token")?.let {
+                        idTokenReceived(it)
+                        return true
+                    }
+                } ?: run {
+                    Log.w(TAG, "Neither authCode nor idToken was able to found")
                 }
                 return super.shouldOverrideUrlLoading(view, request)
             }
@@ -57,8 +67,14 @@ class OktaAuthenticationFragment : WebFragment() {
         parentFragmentManager.popBackStack()
     }
 
+    private fun idTokenReceived(idToken: String) {
+        viewModel.setIdToken(idToken)
+        parentFragmentManager.popBackStack()
+    }
+
     private fun signoutSuccessful() {
         viewModel.clearAuthCode()
+        viewModel.clearIdToken()
     }
 
     override fun onDestroy() {
@@ -73,6 +89,15 @@ class OktaAuthenticationFragment : WebFragment() {
         @JvmStatic
         fun newLoginInstance(): OktaAuthenticationFragment {
             val oktaAuthorizeUrl = buildOktaAuthorizeUrl()
+            return OktaAuthenticationFragment().apply {
+                arguments = Bundle().apply {
+                    putString(URL, oktaAuthorizeUrl)
+                }
+            }
+        }
+
+        fun newImplicitLoginInstance(nonce: String): OktaAuthenticationFragment {
+            val oktaAuthorizeUrl = buildImplicitOktaAuthorizeUrl(nonce)
             return OktaAuthenticationFragment().apply {
                 arguments = Bundle().apply {
                     putString(URL, oktaAuthorizeUrl)
@@ -112,6 +137,19 @@ class OktaAuthenticationFragment : WebFragment() {
             return builder.toString()
         }
 
+        private fun buildImplicitOktaAuthorizeUrl(nonce: String): String {
+            val builder =
+                StringBuilder("https://${notNullOrEmpty(BuildConfig.OKTA_DOMAIN, "OKTA_DOMAIN")}/oauth2/v1/authorize").apply {
+                    append("?client_id=${notNullOrEmpty(BuildConfig.CLIENT_ID, "CLIENT_ID")}")
+                    append("&response_type=id_token")
+                    append("&scope=${notNullOrEmpty(BuildConfig.SCOPES, "SCOPES")}")
+                    append("&redirect_uri=${notNullOrEmpty(BuildConfig.SIGN_IN_REDIRECT_URI, "SIGN_IN_REDIRECT_URI")}")
+                    append("&state=${notNullOrEmpty(BuildConfig.OKTA_STATE, "OKTA_STATE")}")
+                    append("&nonce=${nonce}")
+                }
+            return builder.toString()
+        }
+
         fun buildOktaLogoutUrl(): String{
             val builder =
                 StringBuilder("https://${notNullOrEmpty(BuildConfig.OKTA_DOMAIN, "OKTA_DOMAIN")}/login/signout")
@@ -119,3 +157,36 @@ class OktaAuthenticationFragment : WebFragment() {
         }
     }
 }
+
+
+private fun Uri.getParam(param: String): String? {
+    return fragment?.let { currentFragment ->
+        Log.d(TAG, "Fragment: $currentFragment")
+        // The fragment is a string like "id_token=eyJraWQiOiJyS0px&access_token=..."
+
+        val params = mutableMapOf<String, String>()
+        val pairs = currentFragment.split("&")
+        for (pair in pairs) {
+            val parts = pair.split("=")
+            if (parts.size == 2) {
+                val key = Uri.decode(parts[0]) // Decode URL-encoded parts
+                val value = Uri.decode(parts[1])
+                params[key] = value
+            }
+        }
+
+        val paramValue = params[param]
+
+        if (paramValue != null) {
+            Log.d(TAG, "Extracted $param: $paramValue")
+            paramValue
+        } else {
+            Log.e(TAG, "$param not found in fragment")
+            null
+        }
+    } ?: run {
+        Log.e(TAG, "no fragment found")
+        null
+    }
+}
+
